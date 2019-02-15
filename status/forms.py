12475a7521bd18b7ecbba41237f829058a9b3334
Saranraj V <saranraj.v@msil.ad>
2019-02-15 16:17:56 +0530

    added froms in status app

---
+++ status/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Status
+
+class StatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        fields = [
+            'user',
+            'content',
+            'image'
+        ]
+        
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data.get('content')
+        if len(content) > 250:
+            raise forms.ValidationError('content too long')
+        return content
+
+
+    def clean(self, *args, **kwargs):
+        data = self.cleaned_data
+        content = data.get('content', None)
+        if content == '':
+            content = None
+        
+        image = data.get('image', None)
+        if content is None and image is None:
+            raise forms.ValidationError('content or image field are empty')
+        return super().clean(*args, **kwargs)
+
+
+
+    
+    
